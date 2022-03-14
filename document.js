ef6ffd6bbc47e20e77c32c7d596573a17b2b7e12
Matt <md.stone@live.com>
2022-03-14 15:23:19 -0500

    work

--- document.js
+++ document.js
@@ -39,3 +39,25 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+
+function revealImage() {
+  var reveals = document.querySelectorAll(".revealImage");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealImage);
+
+// To check the scroll position on page load
+reveal();
