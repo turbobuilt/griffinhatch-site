0d4285568b6a036aca61a3a8b0003a9e9efe0d44
Matt <md.stone@live.com>
2022-03-23 16:28:06 -0500

    work

--- document.js
+++ document.js
@@ -61,3 +61,16 @@ window.addEventListener("scroll", revealImage);
 
 // To check the scroll position on page load
 reveal();
+
+document.querySelector('#controls').addEventListener('click', (event) => {
+  const $slide = document.querySelector(event.target.getAttribute('href'));
+  if (!$slide) return;
+  
+  if ($slide.scrollIntoViewIfNeeded) {
+    event.preventDefault();
+    $slide.scrollIntoViewIfNeeded();
+  } else if ($slide.scrollIntoView) {
+    event.preventDefault();
+    $slide.scrollIntoView();
+  }
+});
